0064ec786c59539f585974c3aa1647418002dc95
sidoux <mirsidahmed@hotmail.fr>
2021-12-22 20:41:31 +0100

    ajout refresh jwt

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ function generateToken(user) {
   return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '900s' });
 }
 
+function generateTokenRefresh(user) {
+  return jwt.sign(user, process.env.SECRET_REFRESH_TOKEN, { expiresIn: '1y' });
+}
+
 const accessToken = generateToken(user)
 console.log(accessToken)
 
@@ -30,10 +34,33 @@ app.post('/api/login', (req, res) => {
     }
 
   const accessToken = generateToken(user);
+  const refreshToken = generateTokenRefresh(user);
   res.send({
         accessToken,
+        refreshToken
     });
 
+});
+
+app.post('/api/tokenRefresh', (req, res) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.SECRET_REFRESH_TOKEN, (err, user) => {
+    if (err) {
+      return res.sendStatus(401)
+    }
+
+    //vérifier dans la database si l'user est toujours existant/autorisé à utiliser la plateforme
+    delete user.iat;
+    delete user.exp;
+    const refreshedToken = generateToken(user);
+    res.send({
+      accessToken: refreshedToken,
+    });
+  });
 });
 
 function authenticateJWT(req, res, next) {
